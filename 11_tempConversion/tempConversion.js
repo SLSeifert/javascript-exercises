b1653196f9a04c2ee4f187215343c94c79ce7f09
SLSeifert <slseifert1986@gmail.com>
2025-11-11 15:58:19 -0800

    Program temp conversion.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Ftemp) {
+  return Math.round((Ftemp-32) * (5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Ctemp) {
+  return Math.round((Ctemp * 9/5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
