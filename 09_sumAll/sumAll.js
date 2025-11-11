56a911b24b4fb7a25eb7b278d1a0f75fa1e0d675
SLSeifert <slseifert1986@gmail.com>
2025-11-11 10:36:53 -0800

    Sanatize input for sumAll excercise.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    if (a<0 || b<0) return "ERROR";
+    
+    total = 0
+    if (a>b) {
+        start = b;
+        stop = a;
+    } else {
+        start = a;
+        stop = b;
+    }
+    for (let i = start; i <= stop; i++) {
+        total += i;
+    
+    }
+    
+    return total; 
 
 };
 
